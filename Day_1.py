349cb9b3d4f546a3a5e67c0723a59cb8ce790e83
ChandakaYugandhar <98861499+ChandakaYugandhar@users.noreply.github.com>
2022-02-02 10:06:55 +0530

    Update Day_1.py

--- Day_1.py
+++ Day_1.py
@@ -1,1 +1,7 @@
-# Day-1-Assignment-
+
+#take input as numbers and print total sum
+num = [int(i) for i in input("Enter the values: ").split()]
+total = 0
+for i in num:
+       total+=i
+print("The sum of all Number is:",total)
